13ae2bc98497b21913e7747d34616b508e03d4a0
TheLooDoR <TheLooDoR@gmail.com>
2020-02-29 11:18:18 +0200

    sass

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import './Home.scss'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div className='Home'>
                 Home Component
                 <Link className="navbar-brand" to="/testHome">Test home</Link>
             </div>
